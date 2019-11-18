e40704c08ddf451f90618816bb7db2f5eb6c3972
lummetry-team <tech@lummetry.ai>
2019-11-18 15:59:12 +0200

    Modificari de nume si tag

--- src/frm_cedicrom_unit.pas
+++ src/frm_cedicrom_unit.pas
@@ -14,55 +14,55 @@ type
 
   Tfrm_cedricrom = class(TForm)
     background: TImage;
-    CheckBox1: TCheckBox;
-    CheckBox10: TCheckBox;
-    CheckBox11: TCheckBox;
-    CheckBox12: TCheckBox;
-    CheckBox13: TCheckBox;
-    CheckBox14: TCheckBox;
-    CheckBox15: TCheckBox;
-    CheckBox16: TCheckBox;
-    CheckBox17: TCheckBox;
-    CheckBox18: TCheckBox;
-    CheckBox19: TCheckBox;
-    CheckBox2: TCheckBox;
-    CheckBox20: TCheckBox;
-    CheckBox21: TCheckBox;
-    CheckBox22: TCheckBox;
-    CheckBox3: TCheckBox;
-    CheckBox4: TCheckBox;
-    CheckBox5: TCheckBox;
-    CheckBox6: TCheckBox;
-    CheckBox7: TCheckBox;
-    CheckBox8: TCheckBox;
-    CheckBox9: TCheckBox;
-    CNP: TEdit;
+    urban: TCheckBox;
+    menopauza: TCheckBox;
+    lauzie: TCheckBox;
+    alaptare: TCheckBox;
+    radioterapie: TCheckBox;
+    chimioterapie: TCheckBox;
+    DIU: TCheckBox;
+    leucoree: TCheckBox;
+    contact: TCheckBox;
+    spontan: TCheckBox;
+    cauterizari: TCheckBox;
+    rural: TCheckBox;
+    biopsii: TCheckBox;
+    cu_leziuni: TCheckBox;
+    fara_leziuni: TCheckBox;
+    roma: TCheckBox;
+    ucraineana: TCheckBox;
+    alta: TCheckBox;
+    benigna: TCheckBox;
+    maligna: TCheckBox;
+    ciclu: TCheckBox;
+    sarcina: TCheckBox;
     CNP1: TEdit;
+    CNP2: TEdit;
     CNP10: TEdit;
     CNP11: TEdit;
     CNP12: TEdit;
-    CNP2: TEdit;
-    CNP3: TEdit;
     CNP4: TEdit;
-    CNP5: TEdit;
+    CNP3: TEdit;
     CNP6: TEdit;
+    CNP5: TEdit;
     CNP7: TEdit;
     CNP8: TEdit;
+    CNP13: TEdit;
     CNP9: TEdit;
-    DateTimePicker1: TDateTimePicker;
-    DateTimePicker2: TDateTimePicker;
-    Edit1: TEdit;
-    Edit10: TEdit;
-    Edit11: TEdit;
-    Edit12: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
-    Edit4: TEdit;
-    Edit5: TEdit;
-    Edit6: TEdit;
-    Edit7: TEdit;
-    Edit8: TEdit;
-    Edit9: TEdit;
+    data_recoltare: TDateTimePicker;
+    data_menstruatie: TDateTimePicker;
+    nume: TEdit;
+    centru: TEdit;
+    medic: TEdit;
+    biopsie_descriere: TEdit;
+    prenume: TEdit;
+    judet: TEdit;
+    localitate: TEdit;
+    strada: TEdit;
+    tel: TEdit;
+    cabinet: TEdit;
+    medic_familie: TEdit;
+    etnie: TEdit;
     Label1: TLabel;
     Title: TLabel;
     Title1: TLabel;
@@ -91,8 +91,9 @@ type
     Title7: TLabel;
     Title8: TLabel;
     Title9: TLabel;
-    procedure CNPChange(Sender: TObject);
-    procedure Edit5Change(Sender: TObject);
+    procedure contactChange(Sender: TObject);
+    procedure CNP1Change(Sender: TObject);
+    procedure stradaChange(Sender: TObject);
     procedure Label1Click(Sender: TObject);
     procedure TitleClick(Sender: TObject);
   private
@@ -120,12 +121,17 @@ begin
 
 end;
 
-procedure Tfrm_cedricrom.CNPChange(Sender: TObject);
+procedure Tfrm_cedricrom.CNP1Change(Sender: TObject);
 begin
 
 end;
 
-procedure Tfrm_cedricrom.Edit5Change(Sender: TObject);
+procedure Tfrm_cedricrom.contactChange(Sender: TObject);
+begin
+
+end;
+
+procedure Tfrm_cedricrom.stradaChange(Sender: TObject);
 begin
 
 end;
